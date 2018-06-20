0483a0e1f76ccfcde1590e0e39e10cbb83af0f0f
MasterRobin <robinjacksonkenyan@gmail.com>
2018-06-20 23:21:40 +0300

    Add files via upload
    Certainly, deep learning requires the ability to learn features automatically from the data, which is generally only possible when lots of training data is available --especially for problems where the input samples are very high-dimensional, like images. However, convolutional neural networks --a pillar algorithm of deep learning-- are by design one of the best models available for most "perceptual" problems (such as image classification), even with very little data to learn from. Training a convnet from scratch on a small image dataset will still yield reasonable results, without the need for any custom feature engineering. Convnets are just plain good. They are the right tool for the job.

---
+++ Cat_and_Dog_Classifier.py
@@ -0,0 +1,188 @@
+
+# coding: utf-8
+
+
+
+#Training a small network from scratch (as a baseline)
+#using the bottleneck features of a pre-trained network
+#fine-tuning the top layers of a pre-trained network
+
+
+
+#a training data directory and validation data directory containing one subdirectory per image class, filled with .png or .jpg images:
+#Directory structure for the dataset
+
+#data/
+ #   train/
+  #      dogs/
+   #         dog001.jpg
+   #         dog002.jpg
+    #        ...
+    #    cats/
+    #        cat001.jpg
+    #        cat002.jpg
+    #       ...
+    #validation/
+    #    dogs/
+     #       dog001.jpg
+      #      dog002.jpg
+      #      ...
+       # cats/
+       #     cat001.jpg
+       #     cat002.jpg
+       #     ...
+
+
+
+from keras.preprocessing.image import ImageDataGenerator
+
+from keras.models import Sequential
+
+from keras.layers import Conv2D, MaxPooling2D
+
+from keras.layers import Activation, Dropout, Flatten, Dense
+
+from keras import backend as K
+
+
+
+
+# dimensions of our images.
+
+img_width, img_height = 150, 150
+
+
+
+train_data_dir = 'data/train'
+
+validation_data_dir = 'data/validation'
+
+nb_train_samples = 2000
+
+nb_validation_samples = 800
+
+epochs = 50
+
+batch_size = 1
+
+
+
+
+if K.image_data_format() == 'channels_first':
+
+    input_shape = (3, img_width, img_height)
+
+else:
+
+    input_shape = (img_width, img_height, 3)
+
+
+
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+
+model.add(Activation('relu'))
+
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+
+
+model.add(Conv2D(32, (3, 3)))
+
+model.add(Activation('relu'))
+
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+
+
+model.add(Conv2D(64, (3, 3)))
+
+model.add(Activation('relu'))
+
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+
+
+model.add(Flatten())
+
+model.add(Dense(64))
+
+model.add(Activation('relu'))
+
+model.add(Dropout(0.5))
+
+model.add(Dense(1))
+
+model.add(Activation('sigmoid'))
+
+
+
+
+model.compile(loss='binary_crossentropy',
+
+              optimizer='rmsprop',
+
+              metrics=['accuracy'])
+
+
+
+# this is the augmentation configuration we will use for training
+
+train_datagen = ImageDataGenerator(
+
+    rescale=1. / 255,
+
+    shear_range=0.2,
+
+    zoom_range=0.2,
+
+    horizontal_flip=True)
+
+
+
+# this is the augmentation configuration we will use for testing:
+
+# only rescaling
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+
+
+
+train_generator = train_datagen.flow_from_directory(
+
+    train_data_dir,
+
+    target_size=(img_width, img_height),
+
+    batch_size=batch_size,
+
+    class_mode='binary')
+
+
+
+validation_generator = test_datagen.flow_from_directory(
+
+    validation_data_dir,
+
+    target_size=(img_width, img_height),
+
+    batch_size = batch_size,
+
+    class_mode = 'binary')
+
+
+
+model.fit_generator(
+        train_generator,
+        steps_per_epoch=10 // batch_size,
+        epochs=200,
+        validation_data=validation_generator,
+        validation_steps=10 // batch_size)
+
+
+
+
+model.save_weights('first_try.h5')  # always save your weights after training or during training
+
